7ccd86ac9dcccb2495ee55b2ec33f22e2d1ee841
Nick Rupp <nickrupp95@gmail.com>
2017-05-01 18:06:03 +0100

    Draw hot pink arena

--- smell.py
+++ smell.py
@@ -3,15 +3,37 @@ from pygame.locals import *
 
 FPS = 200
 
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 300
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 300
+
+LINETHICKNESS = 10
+PADDLESIZE = 50
+PADDLEOFFSET = 20
+
+BLACK = (0,186,0)
+WHITE = (255,69,255)
+global DISPLAYSURF
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 
 def main():
     pygame.init()
-    global DISPLAY_SURF
     FPSCLOCK = pygame.time.Clock()
-    DISPLAY_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Smell')
+    ballX = WINDOWWIDTH/2 - LINETHICKNESS/2
+    ballY = WINDOWHEIGHT/2 - LINETHICKNESS/2
+
+    playerOnePosition = (WINDOWHEIGHT - PADDLESIZE)/2
+    playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE)/2
+
+    paddle1 = pygame.Rect(PADDLEOFFSET, playerOnePosition, LINETHICKNESS, PADDLESIZE)
+    paddle2 = pygame.Rect(WINDOWWIDTH-PADDLEOFFSET-LINETHICKNESS, playerTwoPosition, LINETHICKNESS, PADDLESIZE)
+
+    ball = pygame.Rect(ballX, ballY, LINETHICKNESS, LINETHICKNESS)
+
+    drawArena()
+    # drawPaddle(paddle1)
+    # drawPaddle(paddle2)
+    # drawBall(ball)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -20,5 +42,15 @@ def main():
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
+
+def drawArena():
+    DISPLAYSURF.fill((0,0,0))
+    #Draw outline of arena
+    pygame.draw.rect(DISPLAYSURF, WHITE, ((0,0),(WINDOWWIDTH,WINDOWHEIGHT)), LINETHICKNESS*2)
+    #Draw centre line
+    pygame.draw.line(DISPLAYSURF, WHITE, ((WINDOWWIDTH/2),0),((WINDOWWIDTH/2),WINDOWHEIGHT), (LINETHICKNESS/4))
+# DISPLAYSURF.fill((0,124,124))
 if __name__ == '__main__':
     main()
+
+# drawArena()
