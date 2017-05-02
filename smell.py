eeee46491ea6393eb412ad1de108d30467c03a19
Jack Bittiner <jackbittiner@hotmail.com>
2017-05-02 13:58:32 +0100

    score system added

--- smell.py
+++ smell.py
@@ -25,6 +25,10 @@ def main():
     playerOnePosition = (WINDOWHEIGHT - PADDLESIZE)/2
     playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE)/2
 
+    global BASICFONT, BASICFONTSIZE
+    BASICFONTSIZE = 20
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
     player_1_score = 0
     player_2_score = 0
 
@@ -61,6 +65,8 @@ def main():
         ballDirX = ballDirX * checkHitBall(ball, paddle1, paddle2, ballDirX)
         player_1_score, player_2_score = checkPointScored(paddle1, ball,player_1_score,player_2_score, ballDirX)
         ballDirX, ballDirY = checkForEdgeCollision(ball, ballDirX, ballDirY)
+        display_score_1(player_1_score)
+        display_score_2(player_2_score)
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
@@ -93,17 +99,29 @@ def checkForEdgeCollision(ball, ballDirX, ballDirY):
 
 def checkHitBall(ball, paddle1, paddle2, ballDirX):
     if ballDirX == -1 and paddle1.right == ball.left and paddle1.top < ball.top and paddle1.bottom > ball.bottom:
-        return -2
+        return -1
     elif ballDirX == 1 and paddle2.left == ball.right and paddle2.top < ball.top and paddle2.bottom > ball.bottom:
-        return -2
+        return -1
     else: return 1
 
 def checkPointScored(paddle1, ball,player_1_score, player_2_score, ballDirX):
     if ball.left == LINETHICKNESS:
         player_1_score += 7
-    elif ball.right == WINDOWWIDTH - LINETHICKNESS
+    elif ball.right == WINDOWWIDTH - LINETHICKNESS:
         player_2_score += 7
     return player_1_score, player_2_score
+
+def display_score_1(player_1_score):
+    resultSurf = BASICFONT.render('%s' %(player_1_score), True, WHITE)
+    resultRect = resultSurf.get_rect()
+    resultRect.topleft = (WINDOWWIDTH - 300, 50)
+    DISPLAYSURF.blit(resultSurf, resultRect)
+
+def display_score_2(player_2_score):
+    resultSurf = BASICFONT.render('%s' %(player_2_score), True, WHITE)
+    resultRect = resultSurf.get_rect()
+    resultRect.topleft = (300, 50)
+    DISPLAYSURF.blit(resultSurf, resultRect)
 
 if __name__ == '__main__':
     main()
